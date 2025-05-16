87683a6920fb12ade8d1331a5dfd0858dcdf8aa8
766187397 <766187397@qq.com>
2025-05-16 13:29:53 +0800

    refactor(nest-serve): 调整日志文件存储结构
    - 将日志文件按日期分类存储在单独的文件夹中
    - 修改 filename 配置，使用 "logs/%DATE%/info.log" 等格式
    - 适用于 info、error、debug 和 warn 日志配置

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,7 +1,7 @@
 import * as DailyRotateFile from "winston-daily-rotate-file";
 /** info 日志配置 */
 export const InfoConfig = new DailyRotateFile({
-  filename: "logs/info-%DATE%.log",
+  filename: "logs/%DATE%/info.log",
   datePattern: "YYYY-MM-DD",
   maxSize: "20m", // 单个文件最大 20MB
   maxFiles: "30d", // 保留最近 30 天的日志
@@ -12,7 +12,7 @@ export const InfoConfig = new DailyRotateFile({
 
 /** error 日志配置 */
 export const ErrorConfig = new DailyRotateFile({
-  filename: "logs/error-%DATE%.log",
+  filename: "logs/%DATE%/error.log",
   datePattern: "YYYY-MM-DD",
   maxSize: "20m", // 单个文件最大 20MB
   maxFiles: "30d", // 保留最近 30 天的日志
@@ -23,7 +23,7 @@ export const ErrorConfig = new DailyRotateFile({
 
 /** debug 日志配置 */
 export const DebugConfig = new DailyRotateFile({
-  filename: "logs/debug-%DATE%.log",
+  filename: "logs/%DATE%/debug.log",
   datePattern: "YYYY-MM-DD",
   maxSize: "20m", // 单个文件最大 20MB
   maxFiles: "30d", // 保留最近 30 天的日志
@@ -34,7 +34,7 @@ export const DebugConfig = new DailyRotateFile({
 
 /** warn 日志配置 */
 export const WarnConfig = new DailyRotateFile({
-  filename: "logs/warn-%DATE%.log",
+  filename: "logs/%DATE%/warn.log",
   datePattern: "YYYY-MM-DD",
   maxSize: "20m", // 单个文件最大 20MB
   maxFiles: "30d", // 保留最近 30 天的日志
