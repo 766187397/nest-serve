8d67eddf6ec181b6d37490fbe19f6dc0448d145d
胡尚坤 <766187397@qq.com>
2025-05-24 14:13:25 +0800

    "refactor(default-data): 新增角色实体支持，优化默认数据初始化逻辑，添加角色种子数据并关联用户"

--- src/module/default-data/default-data.module.ts
+++ src/module/default-data/default-data.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
-import { User } from "@/module/users/entities/user.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { defaultData } from "./defaultData.service";
+import { User } from "@/module/users/entities/user.entity";
+import { Role } from "../roles/entities/role.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Role])],
   controllers: [],
   providers: [defaultData],
 })
--- src/module/default-data/defaultData.service.ts
+++ src/module/default-data/defaultData.service.ts
@@ -1,17 +1,22 @@
 import { Injectable, OnApplicationBootstrap } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
-import { User } from "@/module/users/entities/user.entity";
 import { bcryptService } from "@/common/utils/bcrypt-hash";
+import { User } from "@/module/users/entities/user.entity";
+import { Role } from "../roles/entities/role.entity";
 
 @Injectable()
 export class defaultData implements OnApplicationBootstrap {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+
+    @InjectRepository(Role)
+    private readonly roleRepository: Repository<Role>
   ) {}
 
   async onApplicationBootstrap() {
+    await this.seedRoles();
     await this.seedUsers();
   }
 
@@ -21,6 +26,8 @@ export class defaultData implements OnApplicationBootstrap {
     if (count === 0) {
       // 如果没有用户，插入默认数据
       const password = await bcryptService.encryptStr("123456");
+      const admin = await this.roleRepository.findOne({ where: { roleKey: "admin" } }); // 获取角色
+      const web = await this.roleRepository.findOne({ where: { roleKey: "web" } }); // 获取角色
       const users = [
         {
           account: "admin",
@@ -29,14 +36,7 @@ export class defaultData implements OnApplicationBootstrap {
           phone: "18888888888",
           email: "admin@example.com",
           sex: "0",
-        },
-        {
-          account: "john_doe",
-          nickName: "John Doe",
-          phone: "18888888889",
-          email: "john@example.com",
-          password,
-          sex: "0",
+          platform: "admin",
         },
         {
           account: "test",
@@ -48,7 +48,30 @@ export class defaultData implements OnApplicationBootstrap {
           platform: "web",
         },
       ];
+
       await this.userRepository.save(users); // 插入数据
+    }
+  }
+
+  private async seedRoles() {
+    const count = await this.roleRepository.count();
+    if (count === 0) {
+      // 如果没有角色，插入默认数据
+      const roles = [
+        {
+          name: "超级管理员",
+          roleKey: "admin",
+          description: "拥有所有权限",
+          platform: "admin",
+        },
+        {
+          name: "普通用户",
+          roleKey: "web",
+          description: "拥有所有权限",
+          platform: "web",
+        },
+      ];
+      await this.roleRepository.save(roles); // 插入数据
     }
   }
 }
