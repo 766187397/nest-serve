d5a3eda5ad1700da42df7c95a21bf7f6ec9e5b0c
胡尚坤 <766187397@qq.com>
2025-05-31 14:54:30 +0800

    feat(default-data): 新增数据字典初始化功能，扩展默认数据模块支持字典及字典项实体

--- src/module/default-data/default-data.module.ts
+++ src/module/default-data/default-data.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { defaultData } from "./defaultData.service";
 import { User } from "@/module/users/entities/user.entity";
-import { Role } from "../roles/entities/role.entity";
-import { Route } from "../routes/entities/route.entity";
+import { Role } from "@/module/roles/entities/role.entity";
+import { Route } from "@/module/routes/entities/route.entity";
+import { Dictionary } from "@/module/dictionary/entities/dictionary.entity";
+import { DictionaryItem } from "@/module/dictionary/entities/dictionaryItem.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Role, Route])],
+  imports: [TypeOrmModule.forFeature([User, Role, Route, Dictionary, DictionaryItem])],
   controllers: [],
   providers: [defaultData],
 })
--- src/module/default-data/defaultData.service.ts
+++ src/module/default-data/defaultData.service.ts
@@ -3,26 +3,41 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { bcryptService } from "@/common/utils/bcrypt-hash";
 import { User } from "@/module/users/entities/user.entity";
-import { Role } from "../roles/entities/role.entity";
-import { Route } from "../routes/entities/route.entity";
+import { Role } from "@/module/roles/entities/role.entity";
+import { Route } from "@/module/routes/entities/route.entity";
+import { Dictionary } from "@/module/dictionary/entities/dictionary.entity";
+import { DictionaryItem } from "@/module/dictionary/entities/dictionaryItem.entity";
 
 @Injectable()
 export class defaultData implements OnApplicationBootstrap {
   constructor(
+    /** 用户 */
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
 
+    /** 角色 */
     @InjectRepository(Role)
     private readonly roleRepository: Repository<Role>,
 
+    /** 路由 */
     @InjectRepository(Route)
-    private readonly routeRepository: Repository<Route>
+    private readonly routeRepository: Repository<Route>,
+
+    /** 字典 */
+    @InjectRepository(Dictionary)
+    private readonly dictionaryRepository: Repository<Dictionary>,
+
+    /** 字典项*/
+    @InjectRepository(DictionaryItem)
+    private readonly dictionaryItemRepository: Repository<DictionaryItem>
   ) {}
 
   async onApplicationBootstrap() {
     await this.seedRoutes();
     await this.seedRoles();
     await this.seedUsers();
+
+    await this.seedDictionary();
   }
 
   // 插入默认用户数据
@@ -144,6 +159,96 @@ export class defaultData implements OnApplicationBootstrap {
         },
       ];
       await this.routeRepository.save(routes); // 插入数据
+    }
+  }
+
+  /** 数据字典 */
+  private async seedDictionary() {
+    const count = await this.dictionaryRepository.count();
+    if (count === 0) {
+      const dictionaries = [
+        {
+          type: "platform",
+          name: "平台",
+          description: "平台",
+        },
+        {
+          type: "routeType",
+          name: "路由类型",
+          description: "路由类型（目录、菜单、按钮等）",
+        },
+        {
+          type: "publicState",
+          name: "公共状态",
+          description: "公共状态:State的具体值（启用禁用）",
+        },
+        {
+          type: "whether",
+          name: "是/否",
+          description: "是/否：通用的是/否值",
+        },
+      ];
+      // 插入字段项目数据
+      const dictionaryList = await this.dictionaryRepository.save(dictionaries);
+
+      const dictionariesItem = [
+        {
+          category: dictionaryList[0],
+          label: "后台",
+          value: "admin",
+          description: "后台平台标识",
+        },
+        {
+          category: dictionaryList[0],
+          label: "web端",
+          value: "web",
+          description: "前台平台标识",
+        },
+        {
+          category: dictionaryList[1],
+          label: "目录",
+          value: "catalogue",
+          description: "路由类型：目录",
+        },
+        {
+          category: dictionaryList[1],
+          label: "菜单",
+          value: "menu",
+          description: "路由类型：菜单",
+        },
+        {
+          category: dictionaryList[1],
+          label: "按钮",
+          value: "button",
+          description: "路由类型：按钮",
+        },
+        {
+          category: dictionaryList[2],
+          label: "启用",
+          value: "1",
+          description: "公共状态：启用",
+        },
+        {
+          category: dictionaryList[2],
+          label: "禁用",
+          value: "0",
+          description: "公共状态：禁用",
+        },
+        {
+          category: dictionaryList[3],
+          label: "是",
+          value: "1",
+          description: "布尔判断：是",
+        },
+        {
+          category: dictionaryList[3],
+          label: "否",
+          value: "0",
+          description: "布尔判断：否",
+        },
+      ];
+
+      await this.dictionaryItemRepository.save(dictionariesItem); // 插入数据
     }
   }
 }
