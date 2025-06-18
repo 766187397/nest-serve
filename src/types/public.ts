bc7f5ba5d9d7158503d9b2b1a6e0b78b2d4772d5
766187397 <766187397@qq.com>
2025-06-18 09:07:55 +0800

    feat(type): 重构类型定义结构，优化接口返回类型和公共基础类型，添加详细注释

--- src/types/public.ts
+++ src/types/public.ts
@@ -1,8 +1,13 @@
 /** 分页返回数据 */
 export interface PageApiResult<T> {
+  /** 数据 */
   data: T;
+  /** 总数 */
   total: number;
+  /** 总页数 */
   totalPages: number;
+  /** 当前页码 */
   page: number | string;
+  /** 每页条数 */
   pageSize: number | string;
 }
