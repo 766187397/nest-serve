aa1e621953f09b45a52050eaae7b8e0fb09b4fa1
766187397 <766187397@qq.com>
2025-07-17 16:51:51 +0800

    refactor(email): 重构邮件模块 DTO
    - 新增 CreateBaseDto 基础类，提高代码复用性
    - 使用 PartialType 自动生成 UpdateEmailDto，减少重复代码
    - 将公共属性提取到 EmailOptionalDto 中，简化类结构
    - 优化 SendEmail 类，增加 email 字段

--- src/module/email/dto/index.ts
+++ src/module/email/dto/index.ts
@@ -1,14 +1,9 @@
+import { CreateBaseDto } from "@/common/dto/base";
+import { PartialType } from "@nestjs/mapped-types";
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
-export class CreateEmailDto {}
-export class UpdateEmailDto {}
-export class SendEmail {
-  @ApiProperty({ description: "收件人邮箱", example: "766187397@qq.com" })
-  @IsString({ message: "邮箱字符串" })
-  @IsNotEmpty({ message: "邮箱是必填项" }) // 必填校验
-  email: string;
-
+class EmailOptionalDto extends CreateBaseDto {
   @ApiProperty({ description: "邮件标题", example: "测试邮件" })
   @IsString({ message: "标题字符串" })
   @IsNotEmpty({ message: "标题是必填项" })
@@ -18,4 +13,28 @@ export class SendEmail {
   @IsString({ message: "内容字符串" })
   @IsNotEmpty({ message: "内容是必填项" })
   content: string;
+}
+
+/** 创建邮箱模板 */
+export class CreateEmailDto extends EmailOptionalDto {}
+
+/** 更新邮箱模板 */
+export class UpdateEmailDto extends PartialType(EmailOptionalDto) {
+  @ApiProperty({ description: "邮件标题", example: "测试邮件" })
+  @IsOptional()
+  @IsString({ message: "标题字符串" })
+  title?: string;
+
+  @ApiProperty({ description: "邮件内容", example: "这是测试邮件" })
+  @IsOptional()
+  @IsString({ message: "内容字符串" })
+  content?: string;
+}
+
+/** 发送邮件 */
+export class SendEmail extends EmailOptionalDto {
+  @ApiProperty({ description: "收件人邮箱", example: "766187397@qq.com" })
+  @IsString({ message: "邮箱字符串" })
+  @IsNotEmpty({ message: "邮箱是必填项" }) // 必填校验
+  email: string;
 }
