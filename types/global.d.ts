61c3db54bec55d74f7d4a425cfe982e9a494ac20
766187397 <766187397@qq.com>
2026-01-19 09:40:45 +0800

    feat(types): 扩展全局类型声明
    添加全局变量类型声明和增强Express请求类型

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,11 +1,21 @@
 // Express中有一个User的类型，如果不取别名类型会走到他内置的User中
 import { User as MyUser } from '@/module/users/entities/user.entity';
+import { OpenAPIObject } from '@nestjs/swagger';
 
 declare global {
   namespace Express {
     interface Request {
+      /** 当前登录用户信息 */
       userInfo?: MyUser;
+      /** 请求开始时间 */
       startTime?: number;
     }
   }
+  // 全局变量
+  /** 项目URL */
+  var url: string;
+  /** 环境变量文件路径 */
+  var envFilePath: string;
+  /** Swagger文档 */
+  var swaggerDocument: OpenAPIObject;
 }
